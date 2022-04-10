d6aa5a3d05655eee7adb385a3662d96ace3165dd
Skyler Parr <skylerparr@gmail.com>
2022-04-10 11:45:07 -0400

    got dynamically reloading of a library working

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,21 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern fn lib_test(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> *mut c_char {
+pub extern fn apply(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>, fun_name: *mut c_char) -> *mut c_char {
     println!("Hello from the library!");
-    match methods.get("respond") {
+    let raw: CString = unsafe { CString::from_raw(fun_name) };
+    let command = CString::into_string(raw).unwrap();
+    let command_str: &str = &command[..];
+    match methods.get(command_str) {
         Some(f) => {
-            println!("calling the function");
+            println!("calling the function...");
             let c_string = CString::new("print this message please");
             f(CString::into_raw(c_string.unwrap()))
         }
         None => {
-            println!("none found");
-            CString::new("none found").unwrap().into_raw()
+            let response = format!("command {} not found", command);
+            println!("{}", response);
+            CString::new(response).unwrap().into_raw()
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::os::raw::c_char;
+use libloading::Library;
 
-fn call_dynamic(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> Result<*mut c_char, Box<dyn std::error::Error>> {
-    unsafe {
-        let lib = libloading::Library::new("dyno.dll")?;
-        let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> *mut c_char> = lib.get(b"lib_test")?;
-        Ok(func(methods))
-    }
-}
+// fn call_dynamic(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> Result<*mut c_char, Box<dyn std::error::Error>> {
+//     unsafe {
+//         let lib = libloading::Library::new("dyno.dll")?;
+//         let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> *mut c_char> = lib.get(b"lib_test")?;
+//         Ok(func(methods))
+//     }
+// }
 
 fn respond(message: *mut c_char) -> *mut c_char {
     let raw: CString = unsafe { CString::from_raw(message) };
@@ -26,16 +27,41 @@ fn main() {
     let mut methods: HashMap<&str, fn(*mut c_char) -> *mut c_char> = HashMap::new();
     methods.insert("respond", respond);
 
-    let result: CString = unsafe { CString::from_raw(call_dynamic(methods).unwrap()) };
-    let to_print = CString::into_string(result).unwrap();
-    println!("final print");
-    println!("{}", to_print);
+    // let result: CString = unsafe { CString::from_raw(call_dynamic(methods).unwrap()) };
+    // let to_print = CString::into_string(result).unwrap();
+    // println!("final print");
+    // println!("{}", to_print);
 
-    /*
-    let mut line = String::new();
-    println!("Enter your name :");
-    let bytes = std::io::stdin().read_line(&mut line).unwrap();
-    println!("Hello, {}"), line);
-    println!("num bytes, {}", bytes);
-    */
+    loop {
+        let mut line = String::new();
+        println!("Enter your name :");
+        let _bytes = std::io::stdin().read_line(&mut line).unwrap();
+        let command = line.trim().to_owned();
+        //println!("num bytes, {}", bytes);
+        if command == "exit" {
+            break;
+        }
+        let methods_to_call = methods.clone();
+        let lib: Library = match unsafe { libloading::Library::new("dyno.dll") } {
+            Ok(v) => v,
+            Err(_e) => panic!("library dyno not found"),
+        };
+        let _ = call_external(lib, methods_to_call, command);
+
+        // let result: CString = unsafe { CString::from_raw(call_dynamic(methods).unwrap()) };
+        // let to_print = CString::into_string(result).unwrap();
+        // println!("final print");
+        // println!("{}", to_print);
+    }
+    
+}
+
+fn call_external(lib: Library, methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>, command: String) -> Result<*mut c_char, Box<dyn std::error::Error>> {
+    unsafe {
+        let raw_command = CString::new(command).unwrap().into_raw();
+        let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>, * mut c_char) -> *mut c_char> = lib.get(b"apply")?;
+        let result = Ok(func(methods, raw_command));
+        let _ = lib.close();
+        result
+    }
 }
