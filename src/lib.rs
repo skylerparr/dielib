83cdc3a1c516f98066fdd313b40ddfe725f26eb0
Skyler Parr <skylerparr@gmail.com>
2022-04-10 12:06:29 -0400

    continuing along

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,12 @@ use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern fn apply(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>, fun_name: *mut c_char) -> *mut c_char {
-    println!("Hello from the library!");
     let raw: CString = unsafe { CString::from_raw(fun_name) };
     let command = CString::into_string(raw).unwrap();
     let command_str: &str = &command[..];
     match methods.get(command_str) {
         Some(f) => {
-            println!("calling the function...");
-            let c_string = CString::new("print this message please");
+            let c_string = CString::new("This is a fun function");
             f(CString::into_raw(c_string.unwrap()))
         }
         None => {
--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,10 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 use libloading::Library;
 
-// fn call_dynamic(methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> Result<*mut c_char, Box<dyn std::error::Error>> {
-//     unsafe {
-//         let lib = libloading::Library::new("dyno.dll")?;
-//         let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>) -> *mut c_char> = lib.get(b"lib_test")?;
-//         Ok(func(methods))
-//     }
-// }
-
-fn respond(message: *mut c_char) -> *mut c_char {
-    let raw: CString = unsafe { CString::from_raw(message) };
-    let to_print = CString::into_string(raw).unwrap();
-    println!("responding to your request");
-    println!("{}", to_print);
-    let ret_val = to_print.clone(); 
-    let c_string = CString::new(ret_val);
-    CString::into_raw(c_string.unwrap())
-}
 
 fn main() {
-    println!("Hello, world!");
-
     let mut methods: HashMap<&str, fn(*mut c_char) -> *mut c_char> = HashMap::new();
     methods.insert("respond", respond);
-
-    // let result: CString = unsafe { CString::from_raw(call_dynamic(methods).unwrap()) };
-    // let to_print = CString::into_string(result).unwrap();
-    // println!("final print");
-    // println!("{}", to_print);
 
     loop {
         let mut line = String::new();
@@ -42,26 +18,30 @@ fn main() {
             break;
         }
         let methods_to_call = methods.clone();
-        let lib: Library = match unsafe { libloading::Library::new("dyno.dll") } {
-            Ok(v) => v,
-            Err(_e) => panic!("library dyno not found"),
-        };
-        let _ = call_external(lib, methods_to_call, command);
+        let lib: Library = unsafe { libloading::Library::new("dyno.dll") }.unwrap();
+        let (lib, response) = call_external(lib, methods_to_call, command);
+        let _ = lib.close();
 
-        // let result: CString = unsafe { CString::from_raw(call_dynamic(methods).unwrap()) };
-        // let to_print = CString::into_string(result).unwrap();
-        // println!("final print");
-        // println!("{}", to_print);
+        let result: CString = unsafe { CString::from_raw(response.unwrap()) };
+        let to_print = CString::into_string(result).unwrap();
+        println!("{}", to_print);
     }
     
 }
 
-fn call_external(lib: Library, methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>, command: String) -> Result<*mut c_char, Box<dyn std::error::Error>> {
+fn call_external(lib: Library, methods: HashMap<&str, fn(*mut c_char) -> *mut c_char>, command: String) -> (Library, Result<*mut c_char, Box<dyn std::error::Error>>) {
     unsafe {
         let raw_command = CString::new(command).unwrap().into_raw();
-        let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>, * mut c_char) -> *mut c_char> = lib.get(b"apply")?;
+        let func: libloading::Symbol<unsafe extern fn(HashMap<&str, fn(*mut c_char) -> *mut c_char>, * mut c_char) -> *mut c_char> = lib.get(b"apply").unwrap();
         let result = Ok(func(methods, raw_command));
-        let _ = lib.close();
-        result
+        (lib, result)
     }
+}
+
+fn respond(message: *mut c_char) -> *mut c_char {
+    let raw: CString = unsafe { CString::from_raw(message) };
+    let to_print = CString::into_string(raw).unwrap();
+    let ret_val = to_print.clone();
+    let c_string = CString::new(ret_val);
+    CString::into_raw(c_string.unwrap())
 }
